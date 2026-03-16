5650997d7f4a4e9ce7502e718f9a34766cffdc60
Cielo6136 <gallanomarkcielo@gmail.com>
2026-03-16 10:51:03 +0800

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,18 @@
+//Problem 5
+class Item {
+    #discount = 0.1;
 
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+    get finalPrice() {
+        return this.price - (this.price * this.#discount);
+    }
+}
+
+const item1 = new Item("Laptop", 1000);
+
+console.log(item1.name);
+console.log(item1.price); 
+console.log(item1.finalPrice);
