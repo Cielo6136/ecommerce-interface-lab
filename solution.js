696349943f1a45307ff2b69cbdd464d70e8b8043
Cielo6136 <gallanomarkcielo@gmail.com>
2026-03-16 09:46:00 +0800

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,12 @@
 
+// Problem 2:
+function generateIDs(count) {
+    const ids = [];
+    for (let i = 0; i < count; i++) {
+        if (i === 5) {
+            continue;
+        }
+        ids.push(`ID-${i}`);
+    }
+    return ids;
+}
