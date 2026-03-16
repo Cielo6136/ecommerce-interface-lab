027d856d4851f9639fea2c693a83e7d0f241a0a3
Cielo6136 <gallanomarkcielo@gmail.com>
2026-03-16 10:43:24 +0800

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,7 @@
-
+function getTopScorers(playerList) {
+    return playerList
+        .slice(0, 10)
+        .filter(player => player.score > 8)
+        .map(player => player.name)
+        .join(", ");
+}
