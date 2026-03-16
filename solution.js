90cd0a43d71db1ecf40874bb9c477bd40b3a5a57
Cielo6136 <gallanomarkcielo@gmail.com>
2026-03-16 10:38:00 +0800

    Implement calculateTotal function with validation
    Add function to calculate total of numbers with input validation.

--- solution.js
+++ solution.js
@@ -1,1 +1,7 @@
-
+// Problem 3:
+function calculateTotal(...numbers) {
+    if (!numbers.every(num => typeof num === "number")) {
+        throw new TypeError("Invalid input: All arguments must be numbers");
+    }
+    return numbers.reduce((total, num) => total + num, 0);
+}
